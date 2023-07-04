6ec2854faa9895513010e92a597864915754c28f
root <root@ubuntu.myguest.virtualbox.org>
2023-07-05 01:10:34 +0530

    contains all files

---
+++ module/scripts/testscript.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+pwd >> /tmp/filebyscript.txt
+echo "first agr: $1" >> /tmp/filebyscript.txt
+echo "========================" >> /tmp/filebyscript.txt
+ip a >> /tmp/filebyscript.txt
+
